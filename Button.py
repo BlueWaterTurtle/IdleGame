63b99503d84922234ab17b88ba4b4dcbc1e903ff
Todd Bonner <toddkbonner@yahoo.com>
2025-03-24 16:08:25 -0400

    Refactor Button class to simplify text handling.
    Removed unused 'feedback' parameter and redundant logic in button initialization. Renamed attributes for clarity and updated methods to streamline text rendering. These changes improve code readability and maintainability.

--- Button.py
+++ Button.py
@@ -1,22 +1,20 @@
 import pygame
 
 class Button:
-    def __init__(self, text, pos, font, bg="black", feedback=""):
+    def __init__(self, text, pos, font, bg="black"):
         self.x, self.y = pos
         self.font = pygame.font.Font(None, font)
-        if feedback == "":
-            self.feedback = "text"
-        else:
-            self.feedback = feedback
+        self.bg = bg
+        self.text = text
         self.change_text(text, bg)
 
     def change_text(self, text, bg="black"):
-        """Change the text when you click"""
-        self.text = self.font.render(text, True, pygame.Color("white"))
-        self.size = self.text.get_size()
+        """Change the text"""
+        self.text_surf = self.font.render(text, True, pygame.Color("white"))
+        self.size = self.text_surf.get_size()
         self.surface = pygame.Surface(self.size)
         self.surface.fill(bg)
-        self.surface.blit(self.text, (0, 0))
+        self.surface.blit(self.text_surf, (0, 0))
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
 
     def show(self, screen):
@@ -25,6 +23,5 @@ class Button:
     def click(self, event):
         x, y = pygame.mouse.get_pos()
         if self.rect.collidepoint(x, y):
-            self.change_text(self.feedback, bg="red")
             return True
         return False
